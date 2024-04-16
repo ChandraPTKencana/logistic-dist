12ff4d89e56c51ea60865b08ecbbbcaa4f71229e
chandra <chandra>
2024-04-16 10:34:13 +0700

    update20240416_103405

---
+++ _nuxt/useMyFetch.1c7a7187.js
@@ -0,0 +1,1 @@
+import{P as t,_ as c}from"./entry.1c7a5935.js";const u=(e,s)=>t(e,{baseURL:c().public.baseURL,...s},"$0wcHT2tPqX");export{u};
---
+++ _nuxt/useMyFetch.e14e4781.js
@@ -0,0 +1,1 @@
+import{P as t,_ as c}from"./entry.398a383c.js";const u=(e,s)=>t(e,{baseURL:c().public.baseURL,...s},"$0wcHT2tPqX");export{u};
